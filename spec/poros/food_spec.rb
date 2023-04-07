3a2b85c8e7d3aeb0e6613986b1bd2401082f97f1
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-04-07 11:02:58 -0600

    Add facade and poro changes

--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -3,25 +3,24 @@ require "rails_helper"
 RSpec.describe Food do
   it "exists" do
     attrs = {
-      total_hits: 51270
-      # foods: [{
-      #   fdcid: 123456,
-      #   gtin: 123456789012,
-      #   description: "Sweet Potato",
-      #   brand_owner: "Sweet Potato Inc.",
-      #   ingredients: "Sweet Potatoes"
-      # }]
+      total_hits: 51270,
+      foods: [{
+        fdcid: 123456,
+        gtin: 123456789012,
+        description: "Sweet Potato",
+        brand_owner: "Sweet Potato Inc.",
+        ingredients: "Sweet Potatoes"
+      }]
     }
 
     food = Food.new(attrs)
-    
+    # binding.pry
     expect(food).to be_a(Food)
     expect(food.total_hits).to eq(51270)
-    # binding.pry
-    # expect(food.fdcid).to eq(123456)
-    # expect(food.gtin).to eq(123456789012)
-    # expect(food.description).to eq("Sweet Potato")
-    # expect(food.brand_owner).to eq("Sweet Potato Inc.")
-    # expect(food.ingredients).to eq("Sweet Potatoes")
+    expect(food.fdcid).to eq(123456)
+    expect(food.gtin).to eq(123456789012)
+    expect(food.description).to eq("Sweet Potato")
+    expect(food.brand_owner).to eq("Sweet Potato Inc.")
+    expect(food.ingredients).to eq("Sweet Potatoes")
   end
 end
